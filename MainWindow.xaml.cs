1ce8320dba78283c9b5b134da35853d425457015
CaptainMorghan <47458916+CaptainMorghan@users.noreply.github.com>
2019-03-18 10:06:56 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nested_loops
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            for (int j = 0; j < 11; j++)
+            {
+                for (int i = (int)'A'; i < (int)'Z'; i++)
+                {
+                    lblOutput.Content += ((char)i).ToString() + j.ToString() + "\t";
+
+                }
+               
+
+                lblOutput.Content += Environment.NewLine;
+            }
+        }
+    }
+}
